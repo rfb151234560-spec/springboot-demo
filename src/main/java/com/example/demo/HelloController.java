9a0462d2b8feb2475cbfdad2ec5000d1ea4aeedf
Willson <goal151234560@gmail.com>
2026-04-23 09:49:51 +0800

    feat: add info api

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,5 +1,8 @@
 package com.example.demo;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +12,13 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello() {
         return "Hello Git + Spring Boot!";
+    }
+    
+    @GetMapping("/info")
+    public Map<String, Object> info(){
+    	Map<String, Object> data = new HashMap<>();
+    	data.put("name", "姓名");
+    	data.put("project", "spring boot");
+    	return data;
     }
 }
